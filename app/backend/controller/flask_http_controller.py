cf7effe37e919d0d109e583a3c55d64f8ace70a8
Ruben Sanchez Diez <ruben.sanchez@n3uron.com>
2025-12-19 09:30:23 +0100

    Dont refresh on init

--- app/backend/controller/flask_http_controller.py
+++ app/backend/controller/flask_http_controller.py
@@ -4,22 +4,22 @@ from backend.domain.models import LatType, LonType, DistanceType
 from backend.infraestructure.gob_gas_stations_repository import GasStationsRepository
 from backend.infraestructure.math_distance_repository import DistanceRepository
 
-gs = Blueprint('gs', __name__)
+gs = Blueprint("gs", __name__)
 
 gas_station_service = GasStationService(
     GasStationsRepository(),
     DistanceRepository(),
 )
 
-gas_station_service.refresh_data()
 
-@gs.route('/refresh') # type: ignore
+@gs.route("/refresh")  # type: ignore
 def refresh():
     global gas_station_service
     gas_station_service.refresh_data()
-    return jsonify( gas_station_service.collect_json(None)) # type: ignore
+    return jsonify(gas_station_service.collect_json(None))  # type: ignore
 
-@gs.get('/<int:num_of_results>') # type: ignore
+
+@gs.get("/<int:num_of_results>")  # type: ignore
 def get_stations(num_of_results: int | None = None) -> str:
     global gas_station_service
     petrol_type: str | None = request.args.get("petrol", type=str)
@@ -28,6 +28,9 @@ def get_stations(num_of_results: int | None = None) -> str:
     lat: LatType | None = request.args.get("lat", type=LatType)
     lon: LonType | None = request.args.get("lon", type=LonType)
 
-    gas_station_service.get_stations().filter_by_ccaa(ccaa).filter_by_distance(distance, lat, lon).sort_by_price(petrol_type)
+    gas_station_service.get_stations().filter_by_ccaa(ccaa).filter_by_distance(
+        distance, lat, lon
+    ).sort_by_price(petrol_type)
 
-    return jsonify( gas_station_service.collect_json(num_of_results)) # type: ignore
+    # type: ignore
+    return jsonify(gas_station_service.collect_json(num_of_results))
